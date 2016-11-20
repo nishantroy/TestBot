2e184bb9deaae640b251019b670ee5157f72a6d8
Nishant Roy <nroy@gatech.edu>
2016-11-19 23:52:01 -0500

    node test

---
+++ server.js
@@ -0,0 +1,22 @@
+// grab the packages we need
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post('/api/users', function(req, res) {
+    var user_id = req.body.id;
+    var token = req.body.token;
+    var geo = req.body.geo;
+
+    res.send(user_id + ' ' + token + ' ' + geo);
+});
+
+// routes will go here
+
+// start the server
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
