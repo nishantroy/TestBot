fc5b36a846ac6ea85f636efc0706e9225f95535e
Nishant Roy <nroy@gatech.edu>
2016-11-20 16:54:22 -0500

    fix

--- server.js
+++ server.js
@@ -18,14 +18,16 @@ var botID = 'c7f81be0af8cbbbce84ecab26d';
 var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(bodyParser.urlencoded({
+	extended: true
+})); // support encoded bodies
 
 
 app.post('/api/testbot', function(req, res) {
 	var body = req.body;
 	var name = body.name;
 	var msg = body.text;
-	
+
 	var text = "Hello " + name + "! Nice of you to say " + msg;
 
 	if (name != 'TestBot') {
@@ -35,32 +37,30 @@ app.post('/api/testbot', function(req, res) {
 			var loc = words[1].toLowerCase();
 			console.log(loc);
 
-			/*request.post('https://api.groupme.com/v3/bots/post', {
+			request.post('https://api.groupme.com/v3/bots/post', {
 				form: {
 					bot_id: botID,
 					text: "Fetching weather for " + words[1]
 				}
 			}, function (err, res) {
 				console.log(err, res);
-			});*/
+			});
 
 			yw.getSimpleWeather(loc).then(function(res) {
-				// console.log(res);
-			    // var date = new Date(res.date);
-			    // date = date.toDateString();
-			    var out = "On "  + res.date + "it is " + res.condition + " in " + words[1] + ".\n" 
-			    	+ "It is " + res.temperature.value + res.temperature.units + " with a " 
-			    	+ res.wind.value + res.wind.units + " wind.\n" 
-			    	+ "With windchill, it is " + res.windChill.value + res.windChill.units;
-			    console.log("Date: " + res.date + ", temp: " + res.temperature.value);
-				console.log(out);
+				var weather = res.weather;
+				var date = new Date(res.date);
+				date = date.toDateString();
+				var out = "On " + date + ", it is " + weather.condition + " in Atlanta" 
+						+ ".\n" + "It is " + weather.temperature.value + weather.temperature.units 
+						+ " with a " + weather.wind.value + weather.wind.units + " wind.\n" 
+						+ "With windchill, it is " + weather.windChill.value + weather.windChill.units + ".";
 
-			    request.post('https://api.groupme.com/v3/bots/post', {
+				request.post('https://api.groupme.com/v3/bots/post', {
 					form: {
 						bot_id: botID,
-						text: text
+						text: out
 					}
-				}, function (err, reason) {
+				}, function(err, reason) {
 					console.log(err, reason);
 				});
 			});
@@ -72,7 +72,7 @@ app.post('/api/testbot', function(req, res) {
 					bot_id: botID,
 					text: text
 				}
-			}, function (err, res) {
+			}, function(err, res) {
 				console.log(err, res);
 			});
 		}
@@ -84,4 +84,4 @@ app.post('/api/testbot', function(req, res) {
 
 // start the server
 app.listen(port);
-console.log('Server started! At port: ' +  port);
+console.log('Server started! At port: ' + port);
---
+++ test.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var path = require("path");
+var yw = require('weather-yahoo');
+var ans = {};
+
+
+
+
+
+var port = process.env.PORT || 8080;
+var request = require('request');
+
+var bodyParser = require('body-parser');
+
+var botID = 'c7f81be0af8cbbbce84ecab26d';
+
+var app = express();
+
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({
+	extended: true
+})); // support encoded bodies
+var loc = "atlanta, ga";
+
+yw.getSimpleWeather(loc).then(function(res) {
+	console.log(res.weather.temperature.value);
+	var weather = res.weather;
+	var date = new Date(res.date);
+	date = date.toDateString();
+	var out = "On " + date + ", it is " + weather.condition + " in Atlanta" 
+	+ ".\n" + "It is " + weather.temperature.value + weather.temperature.units 
+	+ " with a " + weather.wind.value + weather.wind.units + " wind.\n" + "With windchill, it is " 
+	+ weather.windChill.value + weather.windChill.units + ".";
+
+	request.post('https://api.groupme.com/v3/bots/post', {
+		form: {
+			bot_id: botID,
+			text: out
+		}
+	}, function(err, reason) {
+		console.log(err, reason);
+	});
+	
+}).catch(function(err) {
+	console.log(err);
+});
