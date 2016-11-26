86a733fadc637b974e4c521aa257a148c3392ef4
Nishant Roy <nroy@gatech.edu>
2016-11-25 19:32:18 -0500

    fix scheduler (timezone)

--- scheduler.js
+++ scheduler.js
@@ -4,7 +4,7 @@ const stockTracker = require('./bin/stockTracker');
 var date = new Date();
 var day = date.getDay();
 var hour = date.getHours();
-if ((day > 0 && day < 6) && (hour > 9 && hour < 17)) {
+if ((day > 0 && day < 6) && (hour > 14 && hour < 22)) {
 	stockTracker.checkMyThresholds();
 } else {
 	console.log("It's not time for that!");
